8266670c1fcc3300725f432f992d9aa94eadd281
Jinwook <6kimjinwook@gmail.com>
2023-01-11 17:58:25 +0900

    solved problem of two floor tilemap

--- TileMap.gd
+++ TileMap.gd
@@ -1,11 +1,14 @@
 tool
 extends TileMap
 
-var offset = Vector2(0, -64 * 0.5)
+var offset1f = Vector2(0, -36)
+var offset2f = Vector2(0, -52)
 
 func _ready():
-	#for tile in tile_set.get_tiles_ids().size():
-	#	tile_set.tile_set_texture_offset(tile, offset)
-	for tile in tile_set.get_tiles_ids().size():
+	#print(tile_set.get_tiles_ids().size()) #118 #232
+	for tile in tile_set.get_tiles_ids():
 		tile_set.tile_set_region(tile, Rect2(192, 240, 128, 128))
-		tile_set.tile_set_texture_offset(tile, offset)
+		if tile_set.tile_get_name(tile).find("2f") != -1:
+			tile_set.tile_set_texture_offset(tile, offset2f)
+		else:
+			tile_set.tile_set_texture_offset(tile, offset1f)
--- TileSetScript.gd
+++
@@ -1,8 +0,0 @@
-tool
-extends TileMap
-
-var offset = Vector2(-128 * 1.5, -64 * 4.5)
-
-func _ready():
-	for tile in tile_set.get_tiles_ids().size():
-		tile_set.tile_set_texture_offset(tile, offset)
